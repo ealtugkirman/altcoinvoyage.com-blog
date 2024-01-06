ee38fb346b19089da3fa434da3b78dd3adf50654
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-01-06 06:23:12 +0300

    some changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,96 +1,55 @@
 "use client";
 import React from "react";
-import { useForm } from "react-hook-form";
-import { DribbbleIcon, GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons";
 import Link from "next/link";
-import siteMetadata from "@/src/utils/siteMetaData";
+import { FaXTwitter } from "react-icons/fa6";
+import Logo from "../../../public/logo.png";
+import Image from "next/image";
 
 const Footer = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
-
   return (
-    <footer className="mt-16 rounded-2xl bg-dark dark:bg-accentDark/90 m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
-      <h3 className="mt-16 font-medium dark:font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
-        Interesting Stories | Updates | Guides
-      </h3>
-      <p className="mt-5 px-4 text-center w-full sm:w-3/5 font-light dark:font-medium text-sm sm:text-base">
-        Subscribe to learn about new technology and updates. Join over 5000+
-        members community to stay up to date with latest news.
-      </p>
-
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="mt-6 w-fit sm:min-w-[384px] flex items-stretch bg-light dark:bg-dark p-1 sm:p-2 rounded mx04"
-      >
-        <input
-          type="email"
-          placeholder="Enter your email"
-          {...register("email", { required: true, maxLength: 80 })}
-          className="w-full bg-transparent pl-2 sm:pl-0 text-dark focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
-        />
-
-        <input
-          type="submit"
-          className="bg-dark text-light dark:text-dark dark:bg-light cursor-pointer font-medium rounded px-3 sm:px-5 py-1"
-        />
-      </form>
-      <div className="flex items-center mt-8">
-        <a
-          href={siteMetadata.linkedin}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Reach out to me via LinkedIn"
-          target="_blank"
-        >
-          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.twitter}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Reach out to me via Twitter"
-          target="_blank"
-        >
-          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.github}
-          className="inline-block w-6 h-6 mr-4 fill-light"
-          aria-label="Check my profile on Github"
-          target="_blank"
-        >
-          <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.dribbble}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Check my profile on Dribbble"
-          target="_blank"
-        >
-          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-      </div>
-
-      <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
-        <span className="text-center">
-          &copy;2023 CodeBucks. All rights reserved.
-        </span>
-        <Link
-          href="/sitemap.xml"
-          className="text-center underline my-4 md:my-0"
-        >
-          sitemap.xml
-        </Link>
-        <div className="text-center">
-          Made with &hearts; by{" "}
-          <a href="https://devdreaming.com" className="underline" target="_blank">
-            CodeBucks
-          </a>
+    <footer className= "mt-16 justify-between px-24 py-4 pb-8  bg-dark  dark:bg-slate-900  flex flex-col items-center text-light">
+      <div className="border-b flex w-full justify-between pb-4">
+        <div className="flex space-x-12 items-center">
+          <Image
+            src={Logo}
+            alt="altcoinvyage.com logo"
+            sizes="10vw"
+            width={200}
+            height={200}
+            priority
+          />
+          <div className="flex flex-col">
+            <h1 className="text-2xl"> AltCoinVoyage.com </h1>
+            <p className="text-xl "> Your guide on crypto voyage </p>
+          </div>
         </div>
+        <rightfooter className="flex space-x-20">
+          <div className="relative space-y-6 justify-center font-bold flex-col flex">
+            <span>
+              <Link href="/">Home</Link>
+            </span>
+            <span>
+              <Link href="/categories/all">Categories</Link>
+            </span>
+            <span>
+              <Link href="/news">Latest News</Link>
+            </span>
+          </div>
+          <div className="relative space-y-6 justify-center font-bold flex-col flex">
+            <span>
+              <Link href="/about">About Us</Link>{" "}
+            </span>
+            <span>
+              <Link href="/privacypolicy">Privacy Policy</Link>{" "}
+            </span>
+            <span>
+              <Link href="/contact">Contact Us</Link>{" "}
+            </span>
+          </div>
+        </rightfooter>
+      </div>
+      <div className="text-light mt-4 flex">
+        <p> &copy;2024 altcoinvoyage. All rights reserved.</p>
       </div>
     </footer>
   );
--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
         <div className=" w-16 md:w-24 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-            <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
+            <Image src={profileImg} alt="altocoinvoyage logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
       <div className="flex flex-col items-center" >
         <span className="font-bold dark:font-semibold text-lg md:text-xl">altcoinvoyage.com</span>
