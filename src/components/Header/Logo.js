2d8ee221c4776357a33894def85a5ada454403b9
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-01-06 05:01:22 +0300

    hey

---
+++ src/components/Header/Logo.js
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import Link from "next/link"
+import profileImg from "@/public/profile-img.png"
+
+const Logo = () => {
+  return (
+    <Link href="/" className="flex items-center text-dark dark:text-light">
+        <div className=" w-16 md:w-24 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
+            <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
+        </div>
+      <div className="flex flex-col items-center" >
+        <span className="font-bold dark:font-semibold text-lg md:text-xl">altcoinvoyage.com</span>
+        <span className="font-bold dark:font-semibold text-sm md:text-xs">Your guide on crypto voyage</span>
+     </div>
+    </Link>
+  )
+}
+
+export default Logo
---
+++ src/components/Header/index.js
@@ -0,0 +1,222 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { cx } from "@/src/utils";
+import { FaXTwitter } from "react-icons/fa6";
+import { SunIcon, MoonIcon } from "../Icons";
+import { useThemeSwitch } from "../Hooks/useThemeSwitch";
+import Logo from "../../../public/logo.png";
+
+const Header = () => {
+  const [mode, setMode] = useThemeSwitch();
+  const btnRef = useRef(null);
+  const menuRef = useRef(null);
+
+  function navToggle() {
+    btnRef.current.classList.toggle("open");
+    menuRef.current.classList.toggle("hidden");
+    menuRef.current.classList.toggle("flex");
+  }
+
+  function closeMenu() {
+    btnRef.current.classList.remove("open");
+    menuRef.current.classList.add("hidden");
+    menuRef.current.classList.remove("flex");
+  }
+
+  useEffect(() => {
+    const btn = btnRef.current;
+    btn.addEventListener("click", navToggle);
+
+    return () => {
+      btn.removeEventListener("click", navToggle);
+    };
+  }, []);
+
+  useEffect(() => {
+    const menuLinks = menuRef.current.querySelectorAll("a");
+    menuLinks.forEach((link) => {
+      link.addEventListener("click", closeMenu);
+    });
+
+    return () => {
+      menuLinks.forEach((link) => {
+        link.removeEventListener("click", closeMenu);
+      });
+    };
+  }, []);
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const navbar = document.getElementById("navbar");
+    const sticky = navbar.offsetTop;
+
+    const handleScroll = () => {
+      if (window.pageYOffset >= sticky) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="fixed z-40 top-0 w-full">
+      <div
+        id="navbar"
+        className={`flex  dark:bg-dark bg-light font-myfont flex-col ${
+          isScrolled ? "py-0" : "py-0"
+        } ${isScrolled ? "lg:py-3" : "lg:py-3"}`}>
+        <div className="text-dark dark:text-light mx-32 items-center dark:bg-dark bg-light justify-between hidden lg:flex flex-row ">
+          <div className=" items-center space-x-4  text-2xl flex">
+            <Link className="" href="/">
+              <Image
+                src={Logo}
+                width={80}
+                height={80}
+                alt="altcoinvoyage.com"
+              />
+            </Link>
+            <header className="flex flex-col items-center">
+              <p>Altcoin Voyage</p>
+              <p className="text-xs">your guide on crypro world</p>
+            </header>
+          </div>
+          <div className=" space-x-6 mr-8 flex">
+            <span>
+              <Link href="/">Home</Link>
+            </span>
+            <span>
+              <Link href="/categories/all">Categories</Link>
+            </span>
+            <span>
+              <Link href="/news">Latest News</Link>
+            </span>
+            <span>
+              <Link href="/about">About Us</Link>{" "}
+            </span>
+            <span>
+              {" "}
+              <Link href="/privacypolicy">Privacy Policy</Link>{" "}
+            </span>
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={cx(
+                "w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1",
+                mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+              )}
+              aria-label="theme-switcher">
+              {mode === "light" ? (
+                <MoonIcon className={"fill-dark"} />
+              ) : (
+                <SunIcon className={"fill-dark"} />
+              )}
+            </button>
+          </div>
+
+          <div className="flex flex-row text-blue-500 text-2xl items-center space-x-4  ">
+            <div
+              className="bg-third text-lg text-third rounded-2xl px-3 py-1"
+              aria-label="Toggle Menu">
+              <Link href="/contactUs">Contact Us</Link>
+            </div>
+            <a
+              href="https://twitter.com/the_cognizer"
+              aria-label="Twitter Link"
+              className="dark:text-white text-dark">
+              <FaXTwitter />
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Hamburger Menu */}
+      <div className="flex md:flex-row items-center justify-evenly">
+        <div className="flex z-50 fixed justify-between px-8 bg-light dark:bg-dark items-center min-w-full  lg:hidden pt-20 ">
+          <div className="mt-2 flex py-2 items-center text-dark dark:text-light ">
+            <Link href="/">
+              <Image src={Logo} width={70} height={70} alt="cognitive.com" />
+            </Link>
+            <div className="flex flex-col items-center ml-4" >
+              <span className="text-sm" >altcoinvoyage.com</span>
+              <span className="text-xs" >your guide on crypto world</span>
+         </div>
+          </div>
+          <div className="lg:hidden flex items-center  space-x-6">
+            <button
+              onClick={ () => setMode(mode === "light" ? "dark" : "light") }
+              className={ cx(
+                "w-7 h-7 ease  mt-1 flex items-center justify-center rounded-full p-1",
+                mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+              ) }
+              aria-label="theme-switcher">
+              { mode === "light" ? (
+                <MoonIcon className={ "fill-dark" } />
+              ) : (
+                <SunIcon className={ "fill-dark" } />
+              ) }
+            </button>
+            <button
+              id="menu-btn"
+              ref={btnRef}
+              type="button"
+              className="z-50 mt-2 block hamburger lg:hidden focus:outline-none"
+              aria-label="Toggle Menu">
+              <span className="hamburger-top" />
+              <span className="hamburger-middle" />
+              <span className="hamburger-bottom" />
+            </button>
+          </div>
+        </div>
+        {/* Mobile Menu */}
+        <div
+          id="menu"
+          ref={menuRef}
+          className="fixed font-myfont dark:bg-dark  bg-white z-30 top-0 bottom-0 left-0 flex-col text-center hidden lg:hidden w-2/3 min-h-screen py-1 pt-40 space-y-4 text-xl dark:text-light text-dark">
+          <div className="absolute" />
+          <div className="relative space-y-6 justify-center mx-auto flex-col flex">
+            <span>
+              <Link href="/">Home</Link>
+            </span>
+            <span>
+              <Link href="/categories/all">Categories</Link>
+            </span>
+            <span>
+              <Link href="/news">Latest News</Link>
+            </span>
+            <span>
+              <Link href="/about">About Us</Link>{" "}
+            </span>
+            <span>
+              <Link href="/privacypolicy">Privacy Policy</Link>{" "}
+            </span>
+          </div>
+          <p className="dark:text-light  text-black text-left text-sm mx-8 pt-10">
+            The Cognizer is a publishing platform initiated by CogIST, a
+            cognitive science community from Turkey. On this platform, articles
+            and essays on different topics from different fields of cognitive
+            science are published in a way that would bridge the gap between
+            public audience and experts.
+          </p>
+          <div className="flex text-dark dark:text-light text-2xl flex-row space-x-6 pt-6 justify-center">
+            <a
+              href="https://twitter.com/the_cognizer"
+              aria-label="Twitter Link">
+              <FaXTwitter />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
