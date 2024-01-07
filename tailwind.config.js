66d7df733ce6e3dd628eba74d880741432c6ddf9
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-01-07 21:46:34 +0300

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dark: "#1b1b1b",
+        dark: "#03002e",
         light: "#fff",
         accent: "#7B00D3",
         accentDark: "#ffdb4d",
