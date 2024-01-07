77caf6f9e3897893500b37184beaba5cc140edf0
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-01-07 20:59:50 +0300

    analytics

---
+++ src/app/privacypolicy/page.js
@@ -0,0 +1,130 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex text-left leading-6  font-in items-center mt-12 mb-24 text-dark dark:text-light mx-12  md:mx-60">
+      <div className="space-y-6">
+        <div>
+          <h1 className="text-3xl font-semibold mb-12 text-center">
+            Privacy Policy for altcoinvoyage.com
+          </h1>
+          <p className="mb-2 italic"> Last Updated: [07.01.2024]</p>
+          <p>Welcome to altcoinvoyage.com. </p>
+          <p>
+            altcoinvoyage.com is committed to protecting the privacy of its
+            users. This Privacy Policy outlines the types of personal
+            information we collect when you use our website, as well as how we
+            use, share, and protect this information. By using
+            AltcoinVoyage.com, you agree to the terms of this Privacy Policy.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">1. Information We Collect:</h1>
+          <h2 className="mx-8">1.1. Personal Information:</h2>
+          <p className="mx-12">
+            When you visit AltcoinVoyage.com, we may collect personally
+            identifiable information, such as your name and email address, only
+            if voluntarily provided by you.
+          </p>
+          <h2 className="mx-8">1.2. Non-Personal Information:</h2>
+          <p className="mx-12">
+            We may also collect non-personal information, such as browser type,
+            language preference, referring site, and the date and time of each
+            visitor request. This information is used for statistical purposes
+            and to help improve the user experience of the Site.
+          </p>
+        </div>
+        <div>
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">2. How We Use Your Information:</h1>
+          <h2 className="mx-8">2.1. Personal Information:</h2>
+          <p className="mx-12">
+            We may use your personal information to respond to your inquiries,
+            provide you with requested information, and send you newsletters or
+            other relevant content.
+          </p>
+          <h2 className="mx-8">2.2. Non-Personal Information:</h2>
+          <p className="mx-12">
+            Non-personal information is used to analyze trends, administer the
+            Site, track user movements, and gather demographic information for
+            aggregate use.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">3. Cookies:</h1>
+          <p className="mx-8">
+            AltcoinVoyage.com uses cookies to enhance your experience and for
+            analytics purposes. Cookies are small text files that are stored on
+            your device's browser. You can control or refuse the use of cookies
+            by adjusting your browser settings.
+          </p>
+        </div>
+        <div>
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">4. Third-Party Links:</h1>
+          <p className="mx-8">
+            AltcoinVoyage.com may contain links to third-party websites. These
+            external sites are not governed by this Privacy Policy. We encourage
+            you to review the privacy policies of these websites before
+            providing them with any personal information
+          </p>
+        </div>
+        <div>
+          {" "}
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">5. Information Sharing:</h1>
+          <p className="mx-8">
+            We do not sell, trade, or otherwise transfer your personal
+            information to third parties. This does not include trusted third
+            parties who assist us in operating our website or conducting our
+            business, as long as those parties agree to keep this information
+            confidential.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">6. Security:</h1>
+          <p className="mx-8">
+            AltcoinVoyage.com implements reasonable security measures to protect
+            against unauthorized access, alteration, disclosure, or destruction
+            of your personal information.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">7. Children's Privacy:</h1>
+          <p className="mx-8">
+            AltcoinVoyage.com is not directed at individuals under the age of
+            13. We do not knowingly collect personal information from children.
+            If you are a parent or guardian and believe that your child has
+            provided us with personal information, please contact us.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">8. Changes to the Privacy Policy:</h1>
+          <p className="mx-8">
+            AltcoinVoyage.com may update this Privacy Policy from time to time.
+            We will notify you of any changes by posting the new Privacy Policy
+            on this page. It is your responsibility to review this Privacy
+            Policy periodically.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <h1 className="mx-4 underline text-xl font-semibold mb-2 underline-offset-4">9. Contact Information:</h1>
+          <p className="mx-8">
+            If you have any questions or concerns about this Privacy Policy,
+            please contact us at contact@altcoinvoyage.com.
+          </p>
+        </div>
+        <p className="text-center italic">
+          {" "}
+          By using AltcoinVoyage.com, you agree to the terms outlined in this
+          Privacy Policy.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
