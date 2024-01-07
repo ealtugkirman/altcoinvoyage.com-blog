adb643ff3ce86072cd0323625d23068081a98060
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-01-07 21:04:51 +0300

    alla alla

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,25 @@
-import {allBlogs} from "contentlayer/generated";
+import { allBlogs } from "contentlayer/generated";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
 import FeaturedPosts from "../components/Home/FeaturedPosts";
 import RecentPosts from "../components/Home/RecentPosts";
+import Script from "next/script";
 
 export default function Home() {
-  
   return (
     <main className="flex flex-col items-center justify-center">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-7V291HMZJB" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+  gtag('config', 'G-7V291HMZJB');
+        `}
+      </Script>
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
-
-
     </main>
-  )
+  );
 }
