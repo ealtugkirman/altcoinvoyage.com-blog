9dcf908dba906aa6e60c92f4cff7205b1d29c9c6
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-01-09 16:46:15 +0300

    git push

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -8,7 +8,7 @@ const Logo = () => {
         <div className=" w-16 md:w-24 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
             <Image src={profileImg} alt="altocoinvoyage logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
-      <div className="flex flex-col items-center" >
+      <div className="flex flex-col duration-1000 items-center" >
         <span className="font-bold dark:font-semibold text-lg md:text-xl">altcoinvoyage.com</span>
         <span className="font-bold dark:font-semibold text-sm md:text-xs">Your guide on crypto voyage</span>
      </div>
--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -10,10 +10,10 @@ const FeaturedPosts = ({ blogs }) => {
 
     <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
       <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
-        <BlogLayoutOne blog={sortedBlogs[1]} />
+        <BlogLayoutOne blog={sortedBlogs[4]} />
       </article>
       <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
-                <BlogLayoutTwo blog={sortedBlogs[0]} />
+                <BlogLayoutTwo blog={sortedBlogs[1]} />
 
       </article>
       <article className="col-span-2 sm:col-span-1 row-span-1 relative">
