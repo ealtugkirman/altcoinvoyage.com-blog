572395f0135b897d1c17c36c34ffbcb20eee1dd1
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-01-24 02:57:08 +0300

    heyyo

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -10,14 +10,14 @@ const FeaturedPosts = ({ blogs }) => {
 
     <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
       <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
-        <BlogLayoutOne blog={sortedBlogs[2]} />
+        <BlogLayoutOne blog={sortedBlogs[3]} />
       </article>
       <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
-                <BlogLayoutTwo blog={sortedBlogs[5]} />
+                <BlogLayoutTwo blog={sortedBlogs[9]} />
 
       </article>
       <article className="col-span-2 sm:col-span-1 row-span-1 relative">
-                <BlogLayoutTwo blog={sortedBlogs[7]} />
+                <BlogLayoutTwo blog={sortedBlogs[8]} />
 
       </article>
     </div>
